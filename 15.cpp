d61c6c7656db6f7dceecdab1783360687a935726
Buster <buster@buster.me.uk>
2020-12-18 00:49:11 +0000

    Day 15

---
+++ 15.cpp
@@ -0,0 +1,55 @@
+#include "precompiled.h"
+
+auto regex1 = re::regex(R"(^([^:]+)(?::(\d+),(\d+))?$)");
+auto regex2 = re::regex(R"(^(\d+)(?:,|$)(.*)$)");
+auto times = new int[30000000];
+
+void parts(std::istream &stream, int part) {
+  const int limit = part == 1 ? 2020 : 30000000;
+  for (std::string line; std::getline(stream, line);) {
+    if (auto m = match(regex1, line)) {
+      std::memset(times, '\0', limit * sizeof times[0]);
+      int result = 0;
+      int expected = 0;
+      int time = 0;
+      bool test = matched(m, 2);
+      if (test) {
+        expected = string_to<int>(match_view(m, part == 1 ? 2 : 3, line));
+      }
+      auto rest = match_view(m, 1, line);
+      while (auto m = match(regex2, rest)) {
+        times[result] = time++;
+        result = string_to<int>(match_view(m, 1, rest));
+        rest = match_view(m, 2, rest);
+      }
+      while (time != limit) {
+        int previous_result = result;
+        result = times[previous_result] ? time - times[previous_result] : 0;
+        times[previous_result] = time++;
+      }
+      if (test) {
+        if (result != expected) {
+          std::cout << "Fail, got " << result << ", expected " << expected
+                    << std::endl;
+        }
+      } else {
+        std::cout << result << std::endl;
+      }
+    }
+  }
+}
+
+int main() {
+  std::cout << "Day 13, Part One" << std::endl;
+  for (auto filename : {"test/15", "input/15"}) {
+    if (std::ifstream stream(filename); stream) {
+      parts(stream, 1);
+    }
+  }
+  for (auto filename : {"test/15", "input/15"}) {
+    if (std::ifstream stream(filename); stream) {
+      parts(stream, 2);
+    }
+  }
+  return 0;
+}
