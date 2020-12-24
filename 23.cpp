54876e2dc0a7d903939c000b625faf42ff99e083
Buster <buster@buster.me.uk>
2020-12-24 17:13:09 +0000

    Day 23, faster
    Use a linked list, with a vector of iterators to eliminate searching the list.

--- 23.cpp
+++ 23.cpp
@@ -1,94 +1,61 @@
 #include "precompiled.h"
 #include "split.h"
 #include "stopwatch.h"
+#include <list>
 
 using z = std::size_t;
 using ll = std::int64_t;
 constexpr bool verbose = false;
 
-// adjust window so that [iter + 1, iter + 4) is not split
-void adjust_window(std::span<int> &span, z &offset, auto &iter) {
-  z ok = std::end(span) - iter;
-  if (ok == 2 || ok == 3) {
-    --ok;
-    if (offset > ok) {
-      // move window left
-      std::copy(iter + 1, std::end(span), std::begin(span) - ok);
-      span = {std::begin(span) - ok, std::size(span)};
-      offset = offset - ok;
-    } else {
-      // move window right
-      std::copy(std::begin(span), std::begin(span) + 3u - ok, std::end(span));
-      span = {std::begin(span) + 3u - ok, std::size(span)};
-      offset = offset + 3u - ok;
-    }
-  }
-}
+using z = std::size_t;
 
-auto permute(std::span<int> &span, z iterations) {
+void permute(std::list<int> &list, z size, z iterations) {
   stopwatch timer;
-  z size = std::size(span);
-  z offset = 0;
-  auto current = std::begin(span);
+  // Build list index (map content to iterator)
+  std::vector<std::list<int>::const_iterator> index(size);
+  for (auto iter = std::begin(list); iter != std::end(list); ++iter) {
+    index[*iter - 1] = iter;
+  }
+  // Do the dance
   for (z i = 0; i != iterations; ++i) {
-    int dest = *current;
-    adjust_window(span, offset, current);
-    ++current;
-    if (current == std::end(span)) {
-      current = std::begin(span);
-    }
-    // find destination
+    auto pick_begin = std::next(std::begin(list));
+    auto pick_end = std::next(pick_begin, 3);
+    // Find destination
+    int dest = list.front();
     do {
-      dest = (dest + (size - 1)) % size;
-    } while (std::find(current, current + 3, dest) != current + 3);
-    auto dest_iter = std::ranges::find(span, dest);
-    // move current cups to after destination
-    if (current < dest_iter) {
-      std::rotate(current, current + 3, dest_iter + 1);
-    } else {
-      std::rotate(dest_iter + 1, current, current + 3);
-      current += 3;
-      if (current == std::end(span)) {
-        current = std::begin(span);
-      }
-    }
-    if constexpr (verbose) {
-      if ((i % 65536) == 65535) {
-        std::cout << "Iteration " << i << " done ("
-                  << (i * (100.0 / iterations)) << " %), elapsed "
-                  << timer.stamp() << std::endl;
-      }
-    }
+      dest = 1 + (dest + (size - 2)) % size;
+    } while (std::find(pick_begin, pick_end, dest) != pick_end);
+    auto dest_iter = index[dest - 1];
+    // Move picked cups to after destination
+    list.splice(std::next(dest_iter), list, pick_begin, pick_end);
+    // Move new current cup to front
+    list.splice(std::begin(list), list, std::next(std::begin(list)), std::end(list));
   }
-  if constexpr (verbose) {
-    std::cout << "Permutation done, elapsed " << timer.stamp() << std::endl;
-  }
-  auto iter = std::ranges::find(span, 0);
-  adjust_window(span, offset, iter);
-  return iter;
+  // Move 1 to back
+  list.splice(std::begin(list), list, std::next(index[0]), std::end(list));
+  std::cout << "Permutation done, " << timer.stamp() << std::endl;
 }
 
-std::string part_one(auto &&s) {
-  int array[11];
-  std::span span(std::data(array), 9);
-  std::transform(std::begin(s), std::end(s), std::begin(span),
-    [](char c) { return c - '1'; });
-  auto iter = permute(span, 100);
-  std::ranges::rotate(span, iter);
-  std::string result(8, '\0');
-  std::ranges::transform(
-    span.last(8), std::data(result), [](int n) { return char('1' + n); });
-  return result;
+std::string part_one(auto &&input) {
+  std::list<int> list;
+  std::ranges::transform(input, std::back_insert_iterator(list),
+    [](char c) -> int { return c - '0'; });
+  permute(list, 9, 100);
+  std::string result;
+  std::ranges::transform(list | std::ranges::views::take(8),
+    std::back_insert_iterator(result), [](char c) -> int { return '0' + c; });
+  return std::string(result);
 }
 
-ll part_two(auto &&s) {
-  auto array = std::make_unique<int[]>(1'000'002);
-  std::span span(array.get(), 1'000'000);
-  std::iota(std::transform(std::begin(s), std::end(s), std::begin(span),
-              [](char c) { return c - '1'; }),
-    std::end(span), 9);
-  auto iter = permute(span, 10'000'000);
-  return (ll)(*(iter + 1) + 1) * (ll)(*(iter + 2) + 1);
+ll part_two(auto &&input) {
+  std::list<int> list;
+  std::ranges::transform(input, std::back_insert_iterator(list),
+    [](char c) -> int { return c - '0'; });
+  for (int i = 10; i != 1'000'001; ++i) {
+    list.push_back(i);
+  }
+  permute(list, 1'000'000, 10'000'000);
+  return (ll)(*std::begin(list)) * (ll)(*std::next(std::begin(list)));
 }
 
 int main() {
@@ -104,8 +71,8 @@ int main() {
           } else {
             auto expected = match_view(m, 2, line);
             if (result != expected) {
-              std::cout << "Fail, got " << result << ", expected "
-                        << expected << std::endl;
+              std::cout << "Fail, got " << result << ", expected " << expected
+                        << std::endl;
             }
           }
         }
@@ -113,7 +80,7 @@ int main() {
     }
   }
 
-  // Part Two (brute force, ~40 minutes)
+  // Part Two
   for (auto filename : {"input/23"}) {
     if (std::ifstream stream(filename); stream) {
       for (std::string line; std::getline(stream, line);) {
