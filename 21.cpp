ae2b0ec2277a646edc793b0df73ee261979bdc3b
Buster <buster@buster.me.uk>
2020-12-24 10:23:48 +0000

    Day 21, expand identifiers, add comments

--- 21.cpp
+++ 21.cpp
@@ -22,11 +22,12 @@ void parts(std::istream &stream) {
   std::string result2;
   std::string expected2;
 
-  symbols in_names;
-  symbols aller_names;
+  symbols ingredient_names;
+  symbols allergen_names;
+  // contained_by[allergen]: set of ingredients one of which contains 'allergen'
+  // occurs[ingredient]: total number of occurrences of 'ingredient'
   std::map<z, std::set<z>> contained_by;
   std::vector<int> occurs;
-  std::vector<std::set<z>> might_contain;
 
   for (std::string line; std::getline(stream, line);) {
     if (auto m = match(regex1, line)) {
@@ -35,22 +36,27 @@ void parts(std::istream &stream) {
         expected1 = string_to<int>(match_view(m, 1, line));
         expected2 = match_string(m, 2);
       } else {
-        auto in_view = match_view(m, 3, line);
-        auto aller_view = match_view(m, 4, line);
-        std::set<z> ins;
-        for (auto [in_name] : split(in_view, regex2)) {
-          z in = in_names[std::string(in_name)];
-          ins.insert(in);
-          ++vivify(occurs, in);
+        // Process one line of input
+        auto ingredient_view = match_view(m, 3, line); // ingredient list text
+        auto allergen_view = match_view(m, 4, line);   // allergen list text
+        std::set<z> ingredients; // set of ingredient symbols on this line
+        for (auto [ingredient_name] : split(ingredient_view, regex2)) {
+          z ingredient = ingredient_names[std::string(ingredient_name)];
+          ingredients.insert(ingredient);
+          ++vivify(occurs, ingredient);
         }
-        for (auto [aller_name] : split(aller_view, regex2)) {
-          z aller = aller_names[std::string(aller_name)];
-          auto [j, emplaced] = contained_by.try_emplace(aller, ins);
-          auto &ingredients_with_allergen = j->second;
+        for (auto [allergen_name] : split(allergen_view, regex2)) {
+          z allergen = allergen_names[std::string(allergen_name)];
+          // If contained_by[allergen] does not already exist,
+          // assign to it the set of ingredients on this line
+          auto [j, emplaced] = contained_by.try_emplace(allergen, ingredients);
           if (!emplaced) {
+            // Otherwise, assign to contained_by[allergen] the intersection
+            // of its existing value with the set of ingredients on this line
+            auto &ingredients_containing_allergen = j->second;
             std::set<z> intersect;
-            for (auto i : ingredients_with_allergen) {
-              if (ins.contains(i)) {
+            for (auto i : ingredients_containing_allergen) {
+              if (ingredients.contains(i)) {
                 intersect.insert(i);
               }
             }
@@ -60,29 +66,34 @@ void parts(std::istream &stream) {
       }
     }
   }
+  // Search for an allergen contained by exactly one ingredient and remove the
+  // allergen from all other ingredients; repeat until no allergen found
   bool done;
   do {
     done = true;
-    for (auto &[aller, ins] : contained_by) {
-      if (std::size(ins) == 1) {
-        z in = *std::begin(ins);
-        // 'aller' is contained by exactly one ingredient 'in',
-        // so no other allergen is contained by 'in'
-        for (auto &[other_aller, other_ins] : contained_by) {
-          if (other_aller != aller) {
-            auto iter = other_ins.find(in);
-            if (iter != std::end(other_ins)) {
+    for (auto &[allergen, ingredients] : contained_by) {
+      if (std::size(ingredients) == 1) {
+        z ingredient = *std::begin(ingredients);
+        // 'allergen' is contained by exactly one ingredient 'ingredient',
+        // so no other allergen is contained by 'ingredient'
+        for (auto &[other_allergen, other_ingredients] : contained_by) {
+          if (other_allergen != allergen) {
+            auto iter = other_ingredients.find(ingredient);
+            if (iter != std::end(other_ingredients)) {
               done = false;
-              other_ins.erase(iter);
+              other_ingredients.erase(iter);
             }
           }
         }
       }
     }
   } while (!done);
-  for (const auto &[aller, ins] : contained_by) {
-    for (z in : ins) {
-      occurs[in] = 0;
+
+  // Day 1 result: add together the occurrence counts of all ingredients that
+  // do not contain an allergen
+  for (const auto &[allergen, ingredients] : contained_by) {
+    for (z ingredient : ingredients) {
+      occurs[ingredient] = 0;
     }
   }
   result1 = std::accumulate(std::begin(occurs), std::end(occurs), 0);
@@ -97,19 +108,24 @@ void parts(std::istream &stream) {
     std::cout << result1 << std::endl;
   }
 
+  // Day 2
+
+  // Map each allergen to the ingredient that contains it
   std::vector<std::pair<z, z>> contains;
-  for (const auto &[aller, ins] : contained_by) {
-    if (!std::empty(ins)) {
-      assert(std::size(ins) == 1);
-      contains.emplace_back(aller, *std::begin(ins));
+  for (const auto &[allergen, ingredients] : contained_by) {
+    if (!std::empty(ingredients)) {
+      assert(std::size(ingredients) == 1);
+      contains.emplace_back(allergen, *std::begin(ingredients));
     }
   }
+  // Sort alphabetically by allergen name
   std::sort(std::begin(contains), std::end(contains),
-    [&aller_names](const auto &a, const auto &b) {
-      return aller_names[a.first] < aller_names[b.first];
+    [&allergen_names](const auto &a, const auto &b) {
+      return allergen_names[a.first] < allergen_names[b.first];
     });
-  for (const auto &[aller, in] : contains) {
-    result2 += in_names[in];
+  // Day 2 result: list of ingredient names
+  for (const auto &[allergen, ingredient] : contains) {
+    result2 += ingredient_names[ingredient];
     result2 += ',';
   }
   result2.erase(std::size(result2) - 1);
